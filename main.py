fc2af02e0ec012d3736366b0ceea6eb7bb8fa234
Helen Chucha <helenchucha@gmail.com>
2025-06-15 12:31:39 +0300

    Final version

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ def plants_vs_zombies():
     fight(zombies, plants, res_zombies, res_plants, int(init_forces_z), int(init_forces_p))
     res_z = counting_forces(res_zombies)
     res_p = counting_forces(res_plants)
-    print(res_z)
-    print(res_p)
-    res = result_fight(res_zombies, res_plants, result)
+    print('Вижило зомбі: ' + str(res_z) + ' (Початкові сили: ' + str(init_forces_z) + ')')
+    print('Вижило рослин: ' + str(res_p) + ' (Початкові сили: ' + str(init_forces_p) + ')')
+    res = result_fight(res_zombies, res_plants, init_forces_z, init_forces_p, result)
     print('zombies=' + str(zombies) + ' plants=' + str(plants) + '->' + str(res))
 
 def filling_an_array(arr_int):
@@ -45,7 +45,7 @@ def counting_forces(arr_int, total=0):
     # Calculating the force
     for i in range(len(arr_int)):
         total = total + int(arr_int[i])
-        return  total
+    return  total
 
 def fight(zombies, plants, res_zombies, res_plants, init_forces_z, init_forces_p):
     for z, p in zip_longest(zombies, plants, fillvalue=0):
@@ -59,11 +59,16 @@ def fight(zombies, plants, res_zombies, res_plants, init_forces_z, init_forces_p
             res_zombies.append(0)
             res_plants.append(p - z)
 
-def result_fight(res_zombies: object, res_plants: object, result: bool) -> bool:
+def result_fight(res_zombies: object, res_plants: object, init_forces_z, init_forces_p, result: bool) -> bool:
     if counting_forces(res_zombies) > counting_forces(res_plants):
         result: bool = False
     elif counting_forces(res_zombies) < counting_forces(res_plants):
         result: bool = True
+    elif counting_forces(res_zombies) == counting_forces(res_plants):
+        if init_forces_z > init_forces_p:
+            result: bool = False
+        else:
+            result: bool = True
     return result
 
 def print_hi():
