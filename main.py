01a63ece592cd897e432482740b08a20b2f5040a
Helen Chucha <helenchucha@gmail.com>
2025-06-15 08:02:58 +0300

    Start

---
+++ main.py
@@ -0,0 +1,15 @@
+# Plants vs. Zombies
+# Module name: Task_2
+
+from array import *
+
+def main():
+    print_hi()
+
+
+
+def print_hi():
+    print(f'Привіт!')
+
+if __name__ == '__main__':
+    main()
